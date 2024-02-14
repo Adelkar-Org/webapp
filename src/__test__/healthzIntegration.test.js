0c8c8a5c47f01cf185ebd38eeaf43f7202a4fdc6
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-02-14 18:30:47 -0500

    added wait for database to be initialized

--- src/__test__/healthzIntegration.test.js
+++ src/__test__/healthzIntegration.test.js
@@ -3,8 +3,13 @@ const initialize = require("../app");
 const app = initialize();
 
 describe("/healthz endpoint integration tests", () => {
+  beforeAll(async () => {
+    // Wait for 10 seconds before starting the tests
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+  }, 12000); // Increase the timeout to 15 seconds
+
   test("Test 1 - Healthz endpoint", async () => {
     const response = await request(app).get("/healthz");
     expect(response.statusCode).toBe(200);
-  }); // Add a timeout value of 10000 ms
+  });
 });
