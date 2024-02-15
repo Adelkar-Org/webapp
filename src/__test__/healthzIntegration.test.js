5a80f5cd922c13598f401a825b8216064756651b
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-02-15 13:11:59 -0500

    chaged workflow for mysql manual installation

--- src/__test__/healthzIntegration.test.js
+++ src/__test__/healthzIntegration.test.js
@@ -3,15 +3,49 @@ const initialize = require("../app");
 const app = initialize();
 
 describe("/healthz endpoint integration tests", () => {
-  // beforeAll(async () => {
-  //   await new Promise((resolve) => setTimeout(resolve, 10000));
-  // }, 12000);
-  beforeEach(async () => {
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-  });
+  beforeAll(
+    async () => await new Promise((resolve) => setTimeout(resolve, 4000))
+  );
 
-  test("Test 1 - Healthz endpoint", async () => {
+  // get request for healthz endpoint
+  test("Test 1 - get request for healthz endpoint", async () => {
     const response = await request(app).get("/healthz");
     expect(response.statusCode).toBe(200);
+  });
+
+  // post request for healthz endpoint
+  test("Test 2 - post request for healthz endpoint", async () => {
+    const response = await request(app).post("/healthz");
+    expect(response.statusCode).toBe(405);
+  });
+
+  // put request for healthz endpoint
+  test("Test 3 - put request for healthz endpoint", async () => {
+    const response = await request(app).put("/healthz");
+    expect(response.statusCode).toBe(405);
+  });
+
+  // delete request for healthz endpoint
+  test("Test 4 - delete request for healthz endpoint", async () => {
+    const response = await request(app).delete("/healthz");
+    expect(response.statusCode).toBe(405);
+  });
+
+  // patch request for healthz endpoint
+  test("Test 5 - patch request for healthz endpoint", async () => {
+    const response = await request(app).patch("/healthz");
+    expect(response.statusCode).toBe(405);
+  });
+
+  // get with body request for healthz endpoint
+  test("Test 6 - get with body request for healthz endpoint", async () => {
+    const response = await request(app).get("/healthz").send({ key: "value" });
+    expect(response.statusCode).toBe(400);
+  });
+
+  // get with query request for healthz endpoint
+  test("Test 7 - get with query request for healthz endpoint", async () => {
+    const response = await request(app).get("/healthz?name=healthz");
+    expect(response.statusCode).toBe(400);
   });
 });
