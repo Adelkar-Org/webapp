6ce4fce298e0009b2b737aaf05b094264472130e
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-02-14 20:09:35 -0500

    added time for each test

--- src/__test__/healthzIntegration.test.js
+++ src/__test__/healthzIntegration.test.js
@@ -6,6 +6,9 @@ describe("/healthz endpoint integration tests", () => {
   // beforeAll(async () => {
   //   await new Promise((resolve) => setTimeout(resolve, 10000));
   // }, 12000);
+  beforeEach(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+  });
 
   test("Test 1 - Healthz endpoint", async () => {
     const response = await request(app).get("/healthz");
