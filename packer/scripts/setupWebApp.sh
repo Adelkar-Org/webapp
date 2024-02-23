d0668fe0b0304eaeffe0b5fd9916755e9ba307e9
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-02-22 19:51:27 -0500

    Update setupWebApp.sh

--- packer/scripts/setupWebApp.sh
+++ packer/scripts/setupWebApp.sh
@@ -8,6 +8,7 @@ cd /opt/webapp/
 sudo npm install
 touch .env.development
 cp .env.example .env.development
+cat .env.development
 
 sudo chown -R csye6225:csye6225 /opt/webapp
 sudo chmod +x /opt/webapp/server.js
