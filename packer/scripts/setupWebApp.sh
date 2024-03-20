0da2f74b47f41b39c1cc6fe7b0f9933aa2ea137c
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-03-20 19:33:32 -0400

    added logging folder

--- packer/scripts/setupWebApp.sh
+++ packer/scripts/setupWebApp.sh
@@ -3,6 +3,7 @@
 echo "Installing webapp"
 
 sudo mkdir -p /opt/webapp
+
 sudo unzip /tmp/webapp.zip -d /opt/webapp/
 cd /opt/webapp/
 sudo npm install
@@ -11,6 +12,10 @@ sudo touch .env.development
 
 sudo chown -R csye6225:csye6225 /opt/webapp
 sudo chmod +x /opt/webapp/server.js
+
+sudo mkdir -p /var/log/csye6225
+sudo chown csye6225:csye6225 /var/log/csye6225
+
 
 sudo mv /tmp/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
