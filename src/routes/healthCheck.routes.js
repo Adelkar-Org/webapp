0d6543674317027d65007fb4f9e97f8302bab4e9
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-03-20 19:51:55 -0400

    test logger

--- src/routes/healthCheck.routes.js
+++ src/routes/healthCheck.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const healthCheck = require("../controllers/healthCheck.controller.js");
 const setCommonHeaders = require("../middlewares/setHeaders.js");
+const logger = require("../utils/logger.js");
 
 const router = Router();
 // set all the headers required for the health check endpoint
@@ -10,6 +11,12 @@ router.get("/", healthCheck);
 
 // To handle all other unsupported methods on the health check endpoint
 router.all("/", (req, res) => {
+  logger.error("error");
+  logger.info("info");
+  logger.warn("warn");
+  logger.debug("debug");
+  logger.verbose("verbose");
+  logger.silly("silly");
   res.status(405).send();
 });
 
