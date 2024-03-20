98fa5081c0d23870cc14179d960b6795c8d83e82
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-03-20 18:42:15 -0400

    implement logger and ops agent

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const { config } = require("./src/configs/");
 const initialize = require("./src/app.js");
 const errorHandler = require("./src/middlewares/errorHandler.js");
+const logger = require("./src/utils/logger.js");
 
 // Initialize the app
 const app = initialize();
 
 // Start the server
-app.listen(config.app.port, () =>
-  console.log(`Server listening at http://localhost:${config.app.port}/healthz`)
-);
+app.listen(config.app.port, () => {
+  console.log(
+    `Server listening at http://localhost:${config.app.port}/healthz`
+  );
+  logger.info(
+    "Server listening at http://localhost:${config.app.port}/healthz"
+  );
+  logger.error("error");
+  logger.info("info");
+  logger.warn("warn");
+  logger.debug("debug");
+  logger.verbose("verbose");
+  logger.silly("silly");
+});
 
 // Error handler
 app.use(errorHandler);
